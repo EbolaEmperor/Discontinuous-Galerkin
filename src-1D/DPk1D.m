c287cf42a7b98cfc02c2282999e029cd1312d601
Wenchong Huang <wchung.huang@gmail.com>
2025-09-07 12:58:41 +0800

    Start coding

---
+++ src-1D/DPk1D.m
@@ -0,0 +1,24 @@
+classdef DPk1D
+
+properties
+    ord
+    locDof
+end
+
+methods
+    function obj = DPk1D(k)
+        obj.ord = k;
+        obj.locDof = k + 1;
+    end
+
+    function span = getSpan(obj, lam)
+        span = (lam(:,1) .^ (obj.ord:-1:0)) .* (lam(:,2) .^ (0:obj.ord));
+    end
+
+    function span = diffSpan(obj, lam, h)
+        span = (-1/h) * (obj.ord:-1:0) .* (lam(:,1) .^ [obj.ord-1:-1:0, 0]) .* (lam(:,2) .^ (0:obj.ord)) + ...
+               (1/h) * (0:obj.ord) .* (lam(:,2) .^ [0, 0:obj.ord-1]) .* (lam(:,1) .^ (obj.ord:-1:0));
+    end
+end
+
+end
