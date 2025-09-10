a072f340dcec740682df5ea4a859e1956cd2ca65
Wenchong Huang <wchung.huang@gmail.com>
2025-09-10 14:05:51 +0800

    fixed Nitsche weak BDC

--- src-1D/PoissonIPDG1D.m
+++ src-1D/PoissonIPDG1D.m
@@ -6,9 +6,9 @@ ord = 2;
 Nref = 6;
 h0 = 0.25;
 sigma = 5 * ord * (ord + 1);
-u_exact = @(x) sin(pi * x) + exp(x);
-du_exact = @(x) pi * cos(pi * x) + exp(x);
-f = @(x) (pi^2) * sin(pi * x) - exp(x);
+u_exact = @(x) sin(pi * x) - exp(x)/2;
+du_exact = @(x) pi * cos(pi * x) - exp(x)/2;
+f = @(x) (pi^2) * sin(pi * x) + exp(x)/2;
 x0 = 0;
 x1 = 1;
 
@@ -24,21 +24,23 @@ for cycle = 1 : Nref
 
     K = assembleStiffness1D(fem, grid);
     P = assembleInnerPenalty1D(fem, grid, sigma);
-    % Nitsche weak BDC
-    % B = assembleBdryNitsche1D(fem, grid, sigma);
     A = K + P;
 
-    N = size(A, 1);
-    % strong BDC
-    A(1,1) = 1;
-    A(1,2:end) = 0;
-    A(end,end) = 1;
-    A(end, 1:end-1) = 0;
-
     F = assembleLoadVector1D(fem, grid, f);
-    F(1) = u_exact(x0);
-    F(end) = u_exact(x1);
+
+    % Nitsche weak Dirichlet BDC
+    [Ahat, Fhat] = assembleBdryNitsche1D(fem, grid, sigma, u_exact(x0), u_exact(x1));
+    A = A + Ahat;
+    F = F + Fhat;
     sol = A \ F;
+    
+    % strong Dirichlet BDC
+    % N = size(A, 1);
+    % sol = zeros(N, 1);
+    % sol(1) = u_exact(x0);
+    % sol(end) = u_exact(x1);
+    % F = F - A * sol;
+    % sol(2:end-1) = A(2:end-1,2:end-1) \ F(2:end-1);
 
     [errH1(cycle), errL2(cycle), ~] = ...
         getH1Err1D(fem, grid, sol, u_exact, du_exact);
--- src-1D/assembleBdryNitsche1D.m
+++ src-1D/assembleBdryNitsche1D.m
@@ -1,8 +1,9 @@
-function B = assembleBdry1D(fem, grid, sigma)
+function [B, F] = assembleBdryNitsche1D(fem, grid, sigma, g0, g1)
 
 NT = length(grid) - 1;
 nDof = fem.locDof;
 B = sparse(NT*nDof, NT*nDof);
+F = sparse(NT*nDof, 1);
 
 %% 左端点的边界条件
 h0 = grid(2) - grid(1);
@@ -10,6 +11,7 @@ phi = fem.getSpan([1,0]);
 dphi = -fem.diffSpan([1,0], h0);
 idx = 1:nDof;
 B(idx,idx) = - dphi' * phi - phi' * dphi + (sigma/h0) * (phi' * phi);
+F(idx) = - dphi * g0 + (sigma/h0) * phi * g0;
 
 %% 右端点的边界条件
 h1 = grid(end) - grid(end-1);
@@ -17,5 +19,6 @@ phi = fem.getSpan([0,1]);
 dphi = fem.diffSpan([0,1], h1);
 idx = (NT-1)*nDof + (1:nDof);
 B(idx,idx) = - dphi' * phi - phi' * dphi + (sigma/h1) * (phi' * phi);
+F(idx) = - dphi * g1 + (sigma/h1) * phi * g1;
 
 end
